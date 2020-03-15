8a86de5dcb10c0b4071e9b319901d9844b9c7cf2
Cosimac <xilllll@126.com>
2020-03-15 23:43:15 +0800

    初始化项目

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <div :class="{'has-logo':showLogo}">
-    <logo v-if="showLogo" :collapse="isCollapse" />
+  <div>
     <el-scrollbar wrap-class="scrollbar-wrapper">
       <el-menu
         :default-active="activeMenu"
@@ -12,45 +11,41 @@
         :collapse-transition="false"
         mode="vertical"
       >
-        <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
+        <!-- <sidebar-item
+          v-for="route in routes"
+          :key="route.path"
+          :item="route"
+          :base-path="route.path"
+        /> -->
       </el-menu>
     </el-scrollbar>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import Logo from './Logo'
-import SidebarItem from './SidebarItem'
-import variables from '@/styles/variables.scss'
+import variables from "@/styles/variables.scss";
+// import { mapState } from "vuex";
 
 export default {
-  components: { SidebarItem, Logo },
+  name: "Sidebar",
   computed: {
-    ...mapGetters([
-      'sidebar'
-    ]),
-    routes() {
-      return this.$router.options.routes
-    },
+    // ...mapState([
+    //   'sidebar'
+    // ]),
     activeMenu() {
-      const route = this.$route
-      const { meta, path } = route
-      // if set path, the sidebar will highlight the path you set
+      const route = this.$route;
+      const { meta, path } = route;
       if (meta.activeMenu) {
-        return meta.activeMenu
+        return meta.activeMenu;
       }
-      return path
-    },
-    showLogo() {
-      return this.$store.state.settings.sidebarLogo
-    },
-    variables() {
-      return variables
+      return path;
     },
     isCollapse() {
-      return !this.sidebar.opened
+      return true;
+    },
+    variables() {
+      return variables;
     }
   }
-}
+};
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import getters from './getters'
-import app from './modules/app'
-import settings from './modules/settings'
-import user from './modules/user'
+// import getters from './getters'
+// import app from './modules/app'
+
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  modules: {
-    app,
-    settings,
-    user
+  // modules: {
+  //   app
+  // },
+  state: {
+    sidebar: {
+      opened: true
+    }
   },
-  getters
+  // getters
 })
 
 export default store
