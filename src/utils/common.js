289c1cc324e08c39da79d8268aee144bea4b7ace
baiyi <yi.bai@nascent.cn>
2022-10-14 18:59:12 +0800

    js

---
+++ src/utils/common.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2022-10-14 18:05:04
+ * @LastEditors: Cosima
+ * @LastEditTime: 2022-10-14 18:06:14
+ * @FilePath: \vue-admin\src\utils\common.js
+ */
+export function textThis () {
+  console.log(this, 'this---init');
+}
